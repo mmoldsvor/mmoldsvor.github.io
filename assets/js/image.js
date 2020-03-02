d55243b0f03eb7c1c6dbfec61c57b9fe4cc990ee
Martin Moldsvor <martin@moldsvor.com>
2020-03-02 20:41:56 +0100

    Created Carousel for images

---
+++ assets/js/image.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+   $('.carousel').slick({
+       dots: true,
+       infinite: true,
+       arrows: true,
+       autoplay: true,
+       autoplaySpeed: 5000,
+       centerMode: true
+   })
+});
